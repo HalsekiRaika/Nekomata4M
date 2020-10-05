282e90015813c8c1886e4735159da94eaede3853
ReiRokusanami <reirokusanami.rdh@gmail.com>
2020-10-05 15:35:04 +0900

    Fixed AiringTimeCount. (On Calculated Time is Negative, Display string "Standby".) Changed TestCode. (Hololive to Nijisanji.)

--- lib/Util/CalcAiringTimeCount.dart
+++ lib/Util/CalcAiringTimeCount.dart
@@ -4,12 +4,16 @@ class CalcAiringTimeCount {
     DateTime now  = DateTime.now();
     Duration diff = time.difference(now);
 
-    if (diff.inMinutes <= 60) {
-      return diff.inMinutes.toString() + "分";
-    } else if (diff.inHours <= 24) {
-      return diff.inHours.toString() + "時間";
-    } else if (diff.inDays <= 30) {
-      return diff.inDays.toString() + "日";
+    if (!diff.isNegative) {
+      if (diff.inMinutes <= 60) {
+        return diff.inMinutes.toString() + "分";
+      } else if (diff.inHours <= 24) {
+        return diff.inHours.toString() + "時間";
+      } else if (diff.inDays <= 30) {
+        return diff.inDays.toString() + "日";
+      }
+    } else {
+      return "待機中";
     }
   }
 }
--- lib/Widgets/Action/FuncAggregate.dart
+++ lib/Widgets/Action/FuncAggregate.dart
@@ -2,8 +2,7 @@ import 'package:Nekomata/Constants.dart';
 import 'package:Nekomata/DataBase/Aggregater.dart';
 import 'package:Nekomata/DataBase/Buffer/DataBuffer.dart';
 import 'package:Nekomata/Logger/NekomataLogger.dart';
-import 'package:Nekomata/Widgets/Screens/home/Component/ContentBody.dart';
-import 'package:Nekomata/Widgets/Screens/home/Component/ListCardBuilder.dart';
+import 'package:Nekomata/Widgets/Screens/home/Components/ListCardBuilder.dart';
 import 'package:flutter/material.dart';
 
 class FuncAggregate extends StatelessWidget {
@@ -24,12 +23,12 @@ class FuncAggregate extends StatelessWidget {
 
   void _requestUpcomingSearch() async {
     DataBuffer.structureList.clear();
-    List<String> scheduledLiver = await NekomataDataBase().aggregateScheduledLiver(DataBase.HOLOLIVE);
-    NekomataLogger().printInfo("Detect Live   ", "${scheduledLiver.length} 個のチャンネルがライブをスケジュールしています。");
+    List<String> scheduledLiver = await NekomataDataBase().aggregateScheduledLiver(DataBase.NIJISANJI);
+    NekomataLogger().printInfo("Detect Live    ", "${scheduledLiver.length} 個のチャンネルがライブをスケジュールしています。");
     for (String item in scheduledLiver) {
-      DataBuffer.structureList.addAll(await NekomataDataBase().aggregateData(DataBase.HOLOLIVE, item));
+      DataBuffer.structureList.addAll(await NekomataDataBase().aggregateData(DataBase.NIJISANJI, item));
     }
-    NekomataLogger().printInfo("Store Cache   ", "格納しました。 [ ${DataBuffer.structureList.length} ]/Num of ScheduledLive");
-    //ListCardBuilderState().reload();
+    NekomataLogger().printInfo("Store Cache    ", "格納しました。 [ ${DataBuffer.structureList.length} ]/Num of ScheduledLive");
+    ListCardBuilderState().initState();
   }
 }
